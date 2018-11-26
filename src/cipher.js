aef8601445647f5076e02a55f59a955a172cc2f3
colletcm <collet_cm23@hotmail.com>
2018-11-26 16:34:52 -0500

    modificacion de cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,17 @@
 window.cipher = {
-  // ... 
-};
+let mensaje = document.getElementById('mensajito').value;
+    mensaje = mensaje.toUpperCase();
+let clave = document.getElementById('patron').value;
+    clave = parseInt("clave");
+let resultado = "";
+let frase = "";
+  for(i=0; i<mensaje.length; i++){
+    resultado = charCodeAt(i)
+    if(resultado===32){
+      resultado===32
+    }else{
+      resultado + clave = resultado
+    }
+  }
+ resultado = String.fromCharCode("resultado")
+}

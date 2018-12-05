bb6cd68a32cd67969f0c8e8cdfaa6a125f5b5c1f
colletcm <collet_cm23@hotmail.com>
2018-12-05 12:10:39 -0500

    arreglando el orden de los parametros en Cipher.js, html y css

--- src/cipher.js
+++ src/cipher.js
@@ -1,30 +1,30 @@
 window.cipher = {
-  encode: function(mensaje,clave){
-let resultado = "";
-let mensajeFinal = "";
-  for(let i=0; i<mensaje.length; i++){
-    resultado = mensaje.charCodeAt(i);
-    if(resultado===32){
-      mensajeFinal+=" ";
-    }else{
-      mensajeFinal += String.fromCharCode((resultado-65+clave)%26+65);
+  encode: function (offset, text) {
+    let resultado = "";
+    let mensajeFinal = "";
+    for (let i = 0; i < text.length; i++) {
+      resultado = text.charCodeAt(i);
+      if (resultado === 32) {
+        mensajeFinal += " ";
+      } else {
+        mensajeFinal += String.fromCharCode((resultado - 65 + offset) % 26 + 65);
+      }
+    }
+    return mensajeFinal;
+  },
+  //ahora el 2do metodo de el Objeto cipher
+  decode: function (offset, text) {
+    let resultado = "";
+    let mensajeFinal = "";
+    for (let i = 0; i < text.length; i++) {
+      resultado = text.charCodeAt(i);
+      if (resultado === 32) {
+        mensajeFinal = mensajeFinal + " ";
+      } else {
+        mensajeFinal += String.fromCharCode((resultado - 90 - offset) % 26 + 90);
+        //console.log(mensajeFinal);
+      }
+    }
+    return mensajeFinal;
   }
-}
- return mensajeFinal;
-},
-//ahora el 2do metodo de el Objeto cipher
- decode: function(mensaje,clave){
-   let resultado = "";
-   let mensajeFinal = "";
-      for(let i=0; i<mensaje.length; i++){
-        resultado = mensaje.charCodeAt(i);
-      if(resultado===32){
-        mensajeFinal = mensajeFinal+" ";
-      }else{
-        mensajeFinal += String.fromCharCode((resultado-90-clave)%26+90);
-       //console.log(mensajeFinal);
-      }
-      }
-   return mensajeFinal;
- }
 }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ const mensaje1 = document.getElementById("mensajito1");
 const botonCifrado = document.getElementById("encriptar");
 const mensaje2 = document.getElementById("mensajito2");
 const botonRespuesta = document.getElementById("desencriptar");
-  botonCifrado.addEventListener("click", function(){
-   mensaje2.value = cipher.encode(mensaje1.value,parseInt(offset.value));
-  })
-  botonRespuesta.addEventListener("click", function(){
-  mensaje1.value = cipher.decode(mensaje2.value,parseInt(offset.value));
-  })
+botonCifrado.addEventListener("click", function () {
+  mensaje2.value = cipher.encode(parseInt(offset.value), (mensaje1.value).toUpperCase());
+})
+botonRespuesta.addEventListener("click", function () {
+  mensaje1.value = cipher.decode(parseInt(offset.value), (mensaje2.value).toUpperCase());
+})
